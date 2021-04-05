34856b6654a7960c83f354cc479be31303a600b4
dustin dixon <dustin.a.dixon4@gmail.com>
2021-04-05 16:45:37 -0400

    Collections DAO, Get, + Create methods implemented

---
+++ dotnet/Capstone/DAO/CollectionSqlDAO.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Capstone.Models;
+using Capstone.Security;
+using Capstone.Security.Models;
+
+namespace Capstone.DAO
+{
+    public class CollectionSqlDAO : ICollectionDAO
+    {
+        private readonly string connectionString;
+
+        public CollectionSqlDAO(string dbConnectionString)
+        {
+            connectionString = dbConnectionString;
+        }
+
+        public Collection GetCollections(int collectionId)
+        {
+            Collection collectionsFound = null;
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand("SELECT collection_id, user_id, name FROM collections WHERE user_id = @user_id", conn);
+                    cmd.Parameters.AddWithValue("@user_id", collectionId);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        collectionsFound = GetCollectionFromReader(reader);
+                    }
+                }
+            }
+            catch (SqlException)
+            {
+                throw;
+            }
+
+            return collectionsFound;
+        }
+
+        public Collection CreateCollection(int userId, string name)
+        {
+            int newCollectionId;
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand("INSERT INTO collections (user_id, name) VALUES (@user_id, @name)", conn);
+                    cmd.Parameters.AddWithValue("@user_id", userId);
+                    cmd.Parameters.AddWithValue("@name", name);
+                    newCollectionId = Convert.ToInt32(cmd.ExecuteScalar());
+
+                }
+            }
+            catch (SqlException)
+            {
+                throw;
+            }
+
+            return GetCollections(newCollectionId);
+        }
+
+        private Collection GetCollectionFromReader(SqlDataReader reader)
+        {
+            Collection c = new Collection()
+            {
+                CollectionID = Convert.ToInt32(reader["collection_id"]),
+                UserID = Convert.ToInt32(reader["user_id"]),
+                Name = Convert.ToString(reader["name"]),
+            };
+
+            return c;
+        }
+
+    }
+}
---
+++ dotnet/Capstone/DAO/ICollectionDAO.cs
@@ -0,0 +1,10 @@
+﻿using Capstone.Models;
+
+namespace Capstone.DAO
+{
+    public interface ICollectionDAO
+    {
+        Collection CreateCollection(string name);
+        Collection GetCollections(int id);
+    }
+}
