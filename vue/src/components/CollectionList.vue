4d70766a61bc16546e01c9fad5fd8bee7eaa49ec
wes davis <w.guenther.davis@gmail.com>
2021-04-06 11:39:34 -0400

    created mycollections nav bar

--- vue/src/components/CollectionList.vue
+++ vue/src/components/CollectionList.vue
@@ -1,17 +1,31 @@
 <template>
-  <div>
-    <div
-      class="collection"
-      v-for="collection in collections"
-      :key="collection.id"
-    >
-      {{ collection.name }}
-    </div>
-  </div>
+    <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="selectedItem"
+          color="primary"
+        >
+          <v-list-item
+            v-for="collection in collections"
+            :key="collection.id"
+          >
+            <v-list-item-content>
+              <v-list-item-title v-text="collection.name"/>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      selectedItem: ''
+    }
+  },
   name: "CollectionList",
   props: ["collections"],
 };
--- vue/src/views/MyCollections.vue
+++ vue/src/views/MyCollections.vue
@@ -1,32 +1,42 @@
 <template>
-  <div>
-    <h1>My Collections</h1>
-    <collection-list :collections="collections" />
-  </div>
+  <v-container>
+    <v-row>
+      <v-col align="center">
+        <h1>My Collections</h1>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="3">
+        <collection-list :collections="collections" />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import CollectionList from '../components/CollectionList.vue';
+import CollectionList from "../components/CollectionList.vue";
 import CollectionService from "../services/CollectionService";
 
 export default {
-  name: 'MyCollections',
+  name: "MyCollections",
   components: { CollectionList },
   data() {
     return {
-      collections: [],
-      error: '',
+      collections: [{name: "a", id: 1}, {name: "b", id: 2}],
+      error: "",
     };
   },
   created() {
-    CollectionService.getUserCollections().then((response) => {
-      if (response.status === 200) {
-        this.collections = response.data;
-      }
-    }).catch(error => {
-      console.log(error);
-      this.error = error;
-    });
+    CollectionService.getUserCollections()
+      .then((response) => {
+        if (response.status === 200) {
+          this.collections = response.data;
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        this.error = error;
+      });
   },
 };
 </script>
