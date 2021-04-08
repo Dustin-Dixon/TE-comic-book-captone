ccc8840e81613a4cba6f500a367de7e3ee6eae94
Kat Kuntz <rezonantcheetah@gmail.com>
2021-04-08 14:25:30 -0400

    Hook up collection settings to api endpoint

--- vue/src/components/Collections/SettingsMenu.vue
+++ vue/src/components/Collections/SettingsMenu.vue
@@ -1,5 +1,10 @@
 <template>
-  <v-menu :close-on-content-click="false" :nudge-width="200" offset-x>
+  <v-menu
+    v-model="showMenu"
+    :close-on-content-click="false"
+    :nudge-width="200"
+    offset-x
+  >
     <template v-slot:activator="{ on, attrs }">
       <v-btn
         icon
@@ -12,7 +17,7 @@
     </template>
     <v-card>
       <v-card-text>
-        <v-text-field label="Name" v-model="newCollection.name" />
+        <v-text-field label="Name" v-model="newCollection.name" autofocus />
         <v-checkbox label="Public" v-model="newCollection.public"></v-checkbox>
       </v-card-text>
       <v-card-actions>
@@ -24,24 +29,31 @@
 </template>
 
 <script>
-
-import CollectionService from "@/services/CollectionService.js"
+import CollectionService from "@/services/CollectionService.js";
 
 export default {
-    props: ["collection"],
-    data () {
-        return {
-            newCollection: {name: '', collectionID: 0, public: false}
+  props: ["collection"],
+  data() {
+    return {
+      newCollection: { name: "", collectionID: 0, public: false },
+      showMenu: false,
+    };
+  },
+  methods: {
+    updateCollection() {
+      CollectionService.updateCollectionSettings(this.newCollection).then(
+        (response) => {
+          console.log(response);
+          if (response.status === 201) {
+            // Set the properties inside of this.collection to match response.data
+            // without setting this.collection to a new object
+            Object.assign(this.collection, response.data);
+          }
         }
+      );
+      this.newCollection = {};
+      this.showMenu = false;
     },
-    methods: {
-        updateCollection() {
-            CollectionService.updateCollectionSettings(this.newCollection).then((response) => {
-                if (response.status === 200){
-                    
-                }
-            })
-        }
-    }
-}
+  },
+};
 </script>
