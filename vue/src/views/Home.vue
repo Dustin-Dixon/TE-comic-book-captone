39de7a67261223c1644659305ee302836f8282ca
Kat Kuntz <rezonantcheetah@gmail.com>
2021-04-09 09:16:35 -0400

    Add collection carousel to home page with dummy collection slides

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,5 +1,19 @@
 <template>
   <v-container>
-    <h1 class="text-h1 text-center">Welcome to <span class="red--text">POW</span>er Comics</h1>
+    <h1 class="text-h1 text-center">
+      Welcome to <span class="red--text">POW</span>er Comics
+    </h1>
+    <v-divider />
+    <collection-carousel class="mt-1"/>
   </v-container>
 </template>
+
+<script>
+import CollectionCarousel from '../components/CollectionCarousel.vue';
+
+export default {
+  components: {
+    CollectionCarousel
+  },
+};
+</script>
