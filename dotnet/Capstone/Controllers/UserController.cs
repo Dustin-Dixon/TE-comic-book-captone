0b53c7a96640acf818fe208d5a0e0a4374c46491
spencer schick <spencer.schick@rocketmail.com>
2021-04-12 14:05:42 -0400

    updated deny standardd 100 comics

--- dotnet/Capstone/Controllers/UserController.cs
+++ dotnet/Capstone/Controllers/UserController.cs
@@ -98,7 +98,7 @@ namespace Capstone.Controllers
             int userId = GetUserIdFromToken();
             if (VerifyActiveUserOwnsCollection(id))
             {
-                if (CheckUserRole(userId) || collectionDAO.UserTotalComicCount(userId) < 100 )
+                if (CheckUserRole(userId) || collectionDAO.UserTotalComicCount(userId) < 100  )
                 {
 
 
