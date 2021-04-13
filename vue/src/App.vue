17088606347fc6fd0baeec2fa11ac1cac4a6bd99
wes davis <w.guenther.davis@gmail.com>
2021-04-13 18:11:02 -0400

    display 4 comics in public collection carousel preview

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
-      <v-btn class="mx-1" :to="{ name: 'home' }" icon exact text depressed><v-img src="../public/power_comics_icon.png" height="50px" contain/></v-btn>
+      <v-btn class="mx-1" :to="{ name: 'home' }" icon exact text
+        ><v-img src="../public/power_comics_icon.png" height="50px" contain
+      /></v-btn>
       <v-btn :to="{ name: 'my-collections' }" text v-if="loggedIn"
         >Collections</v-btn
       >
