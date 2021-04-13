e3ee3fdc8524a7c48abfc426a1b3a99199b2c0ba
dustin dixon <dustin.a.dixon4@gmail.com>
2021-04-13 11:05:17 -0400

    result models created for API issue response

---
+++ dotnet/Capstone/Services/CVSingleIssueResponse.cs
@@ -0,0 +1,14 @@
+﻿using Capstone.Models;
+
+namespace Capstone.Services
+{
+    public class CVSingleIssueResponse
+    {
+        public string Error { get; set; }
+        public int Limit { get; set; }
+        public int NumberOfPageResults { get; set; }
+        public int NumberOfTotalResults { get; set; }
+        public int StatusCode { get; set; }
+        public ApiResult Results { get; set; }
+    }
+}
