9fbecb5fbbd5b1274bf3cd8ddca88f55576cb43b
wes davis <w.guenther.davis@gmail.com>
2021-04-13 16:12:51 -0400

    add collection name and username to public collection view

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
   <v-container>
     <v-img :src="require('@/assets/power_comics_logo.png')" max-height="400px" contain></v-img>
+    <v-row>
+      <v-col>
+        <h3 class="text-h3 text-center">Public Collections</h3>
+      </v-col>
+    </v-row>
     <v-divider />
     <collection-carousel class="mt-1" />
   </v-container>
--- vue/src/views/PublicCollection.vue
+++ vue/src/views/PublicCollection.vue
@@ -1,33 +1,46 @@
 <template>
-    <v-container>
-        <collection-display :comics="comics" />
-    </v-container>
+  <v-container>
+    <v-row>
+      <v-col>
+        <h3 class="text-h3 text-center">{{ collection.name }}</h3>
+        <h4 class="text-h4 text-center">By: {{ collection.username }}</h4>
+      </v-col>
+    </v-row>
+    <collection-display :comics="comics" />
+  </v-container>
 </template>
 
 <script>
-
-import CollectionDisplay from "../components/Collections/CollectionDisplay.vue"
-import CollectionService from "../services/CollectionService.js"
+import CollectionDisplay from "../components/Collections/CollectionDisplay.vue";
+import CollectionService from "../services/CollectionService.js";
 export default {
-    components: {CollectionDisplay, CollectionService},
-    data () {
-        return {
-            comics: [],
-        }
+  components: { CollectionDisplay, CollectionService },
+  data() {
+    return {
+      comics: [],
+      collection: [],
+    };
+  },
+  computed: {
+    collectionID() {
+      return this.$route.params.id;
     },
-    computed: {
-        collectionID() {
-            return this.$route.params.id;
+  },
+  created() {
+    CollectionService.getPubliCollectionFromID(this.collectionID).then(
+      (response) => {
+        if (response.status === 200) {
+          this.collection = response.data;
         }
-    },
-    created () {
-        CollectionService.getComicsInPublicCollection(this.collectionID).then(
-        (response) => {
-          if (response.status === 200) {
-            this.comics = response.data;
-          }
+      }
+    );
+    CollectionService.getComicsInPublicCollection(this.collectionID).then(
+      (response) => {
+        if (response.status === 200) {
+          this.comics = response.data;
         }
-      );
-    }
-}
+      }
+    );
+  },
+};
 </script>
