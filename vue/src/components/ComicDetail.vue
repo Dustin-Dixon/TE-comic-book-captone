a565479b5994f97aa0f5effede71b2c1f149a173
wes davis <w.guenther.davis@gmail.com>
2021-04-13 10:58:15 -0400

    create comicDetail component, not yet implemented

---
+++ vue/src/components/ComicDetail.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-card
+    v-model="showDetail"
+    :close-on-content-click="false"
+    :nudge-width="200"
+    offset-x
+    elevation="2"
+  >
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn
+        icon
+        v-on="on"
+        v-bind="attrs"
+        @click="newCollection = { ...comic }"
+      >
+        <v-icon>mdi-info-outline</v-icon>
+      </v-btn>
+    </template>
+    <v-img :src="comic.image.mediumUrl"></v-img>
+    <v-card-title align="center">{{ comic.name }}</v-card-title>
+    <v-card-text>
+      <div>Issue Number: {{ comic.issueNumber }}</div>
+      <div>Cover Date: {{ comic.coverDate }}</div>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ["comic"],
+  data() {
+    return {
+      showDetail: false,
+      comicDetails: {
+        name: "",
+        comicID: "",
+        issueNumber: "",
+        image: { mediumUrl: "" },
+        coverDate: "",
+      },
+    };
+  },
+};
+</script>
