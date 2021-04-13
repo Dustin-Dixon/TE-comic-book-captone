a53961be5e240358daaa586c820760ae4f664d33
Kat Kuntz <rezonantcheetah@gmail.com>
2021-04-13 19:21:54 -0400

    Don't register CollectionService as a component

--- vue/src/components/Collections/CollectionPreview.vue
+++ vue/src/components/Collections/CollectionPreview.vue
@@ -4,10 +4,10 @@
       <v-row>
         <v-col>
           <h3 class="text-h3 text-center">{{ collection.name }}</h3>
-          <h4 class="text-h4 text-center">By: {{collection.username}}</h4>
+          <h4 class="text-h4 text-center">By: {{ collection.username }}</h4>
         </v-col>
       </v-row>
-      <v-divider class="my-5"/>
+      <v-divider class="my-5" />
       <v-row>
         <collection-display :comics="firstFourComics" :showInfo="false" />
       </v-row>
@@ -16,29 +16,30 @@
 </template>
 
 <script>
-import CollectionDisplay from "./CollectionDisplay.vue"
-import CollectionService from "../../services/CollectionService.js"
+import CollectionDisplay from "./CollectionDisplay.vue";
+import CollectionService from "../../services/CollectionService.js";
+
 export default {
-  components: { CollectionDisplay, CollectionService},
+  components: { CollectionDisplay },
   props: ["collection"],
   data() {
     return {
       comics: [],
-    }
+    };
   },
   computed: {
     firstFourComics() {
       return this.comics.slice(0, 4);
-    }
+    },
   },
   created() {
-    CollectionService.getComicsInPublicCollection(this.collection.collectionID).then(
-      (response) => {
-        if (response.status === 200){
-          this.comics = response.data;
-        }
+    CollectionService.getComicsInPublicCollection(
+      this.collection.collectionID
+    ).then((response) => {
+      if (response.status === 200) {
+        this.comics = response.data;
       }
-    )
-  }
+    });
+  },
 };
 </script>
