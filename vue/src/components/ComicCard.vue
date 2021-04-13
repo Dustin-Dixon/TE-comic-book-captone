3e2530ccb35d88b1bc70d75b3fbf0dc3b3f6f775
wes davis <w.guenther.davis@gmail.com>
2021-04-13 12:14:26 -0400

    implement comicDetail component

--- vue/src/components/ComicCard.vue
+++ vue/src/components/ComicCard.vue
@@ -1,14 +1,22 @@
 <template>
   <v-card>
     <v-img :src="comic.image.mediumUrl" :height="`${imageHeight}px`"></v-img>
-    <h6 class="text-center">{{ comic.name }}</h6>
-    <p class="text-center">Issue Number: {{ comic.issueNumber }}</p>
+    <!-- <v-card-text>
+    <div class="text-center">{{ comic.name }} #{{comic.issueNumber}}</div>
+    </v-card-text> -->
+    <v-card-actions>
+      <comic-detail :comic="comic" />
+    </v-card-actions>
   </v-card>
 </template>
 
 <script>
+
+import ComicDetail from "./ComicDetail.vue"
+
 export default {
   props: ["comic"],
+  components: {ComicDetail},
   computed: {
     imageHeight() {
       switch (this.$vuetify.breakpoint.name) {
