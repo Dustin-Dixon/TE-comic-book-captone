bd9e0aecb34fcb9a996e5d51700ffbce76d488ea
spencer schick <spencer.schick@rocketmail.com>
2021-04-15 16:39:03 -0400

    about us minus kat

--- vue/src/views/AboutUs.vue
+++ vue/src/views/AboutUs.vue
@@ -7,17 +7,34 @@
           </v-col>
           <v-col align="center">
               <h3 class="text-h3">Wesley Davis</h3>
-              <p class="text-h6">Info here</p>
+              <p class="text-h6"> Favorite Comic: Hellboy </p>
+              <p class="text-h6"> Favorite Cereal: Captain Crunch </p>
+              <p class="text-h6"> Bedtime: Too Late </p>
+              <p class="text-h6"> Superpower: Invulnerability </p>
+              <p class="text-h6"> Alias: Pippin The Hedgehog </p>
+
           </v-col>
       </v-row>
       <v-row>
           <v-col align="center">
               <h3 class="text-h3">Spencer Schick</h3>
-              <p class="text-h6">Info here</p>
+              <p class="text-h6">Favorite Comic: Green Lantern</p>
+              <p class="text-h6"> Favorite Cereal: Reeses Puffs </p>
+              <p class="text-h6"> Bedtime: During Joe's Lectures </p>
+              <p class="text-h6"> Superpower: Memory Loss </p>
+              <p class="text-h6"> Alias: Dr.Doverylittle </p>
           </v-col>
           <v-col align="center">
               <h3 class="text-h3">Dustin Dixon</h3>
-              <p class="text-h6">Info here</p>
+              <p class="text-h6">Favorite Comic: Green Lantern</p>
+              <p class="text-h6">Favorite Cereal: Cinnamon Toast Crunch</p>
+              <p class="text-h6">Bedtime: 10:30pm weekdays/before sunrise on the weekends</p>
+              <p class="text-h6">Superpower: Feathered Flight </p>
+              <p class="text-h6">Alias: Masked Macaw</p>
+
+
+
+
           </v-col>
       </v-row>
   </v-container>
