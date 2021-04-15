e70968072bf5bc6fe419b0b0ee1c576e9a9b9f4d
wes davis <w.guenther.davis@gmail.com>
2021-04-15 18:43:38 -0400

    finish statistics component and style

--- vue/src/components/Collections/CollectionStatistics.vue
+++ vue/src/components/Collections/CollectionStatistics.vue
@@ -1,8 +1,18 @@
 <template>
   <v-container>
-    <h3 class="text-center">Collection Stats</h3>
+    <h3 class="text-center text-h6">Collection Stats</h3>
     <template v-if="hasCollection">
-      <h6>Total Comics In Collection: {{ stats.comicCount }}</h6>
+      <h6 class="text-nav">Total Comics In Collection: {{ stats.comicCount }}</h6>
+      <v-divider class="my-1"></v-divider>
+      <h3 class="text-center text-h6">Top 5 Characters</h3>
+      <div v-for="characterCount in topFiveCharacters" :key="characterCount.character.id">
+        <h6 class="text-nav">{{characterCount.character.name}}: {{characterCount.count}}</h6>
+      </div>
+      <v-divider class="my-1"></v-divider>
+      <h3 class="text-center text-h6">Top 5 Creators</h3>
+      <div v-for="creatorCount in topFiveCreators" :key="creatorCount.creator.id">
+        <h6 class="text-nav">{{creatorCount.creator.name}}: {{creatorCount.count}}</h6>
+      </div>
     </template>
   </v-container>
 </template>
@@ -25,6 +35,12 @@ export default {
     ...mapState(["user"]),
     comicCount() {
       return this.user.comicCount;
+    },
+    topFiveCharacters() {
+      return this.stats.characters.slice(0, 5);
+    },
+    topFiveCreators() {
+      return this.stats.creators.slice(0, 5);
     },
   },
   methods: {
