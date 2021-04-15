d116938fb574f08d37edb9d63d26c602cbe92e2c
dustin dixon <dustin.a.dixon4@gmail.com>
2021-04-15 17:41:10 -0400

    updated about us page

--- vue/src/views/AboutUs.vue
+++ vue/src/views/AboutUs.vue
@@ -1,41 +1,40 @@
 <template>
   <v-container>
       <v-row>
-          <v-col align="center">
-              <h3 class="text-h3">Kat Kuntz</h3>
-              <p class="text-h6">Info here</p>
+          <v-col cols="6">
+              <v-img src="../assets/KatComic.png" max-height="200px" contain/>
+              <h3 align="center" class="text-h3">Kat Kuntz</h3>
+              <p align="center" class="text-h6">Info here</p>
           </v-col>
-          <v-col align="center">
-              <h3 class="text-h3">Wesley Davis</h3>
-              <p class="text-h6"> Favorite Comic: Hellboy </p>
-              <p class="text-h6"> Favorite Cereal: Captain Crunch </p>
-              <p class="text-h6"> Bedtime: Too Late </p>
-              <p class="text-h6"> Superpower: Invulnerability </p>
-              <p class="text-h6"> Alias: Pippin The Hedgehog </p>
-
+          <v-col cols="6">
+              <v-img src="../assets/Wes.png" max-height="200px" contain/>
+              <h3 align="center" class="text-h3">Wesley Davis</h3>
+              <p align="center" class="text-h6"> Favorite Comic: Hellboy </p>
+              <p align="center" class="text-h6"> Favorite Cereal: Captain Crunch </p>
+              <p align="center" class="text-h6"> Bedtime: Too Late </p>
+              <p align="center" class="text-h6"> Superpower: Invulnerability </p>
+              <p align="center" class="text-h6"> Alias: Pippin The Hedgehog </p>
           </v-col>
       </v-row>
       <v-row>
-          <v-col align="center">
-              <h3 class="text-h3">Spencer Schick</h3>
-              <p class="text-h6">Favorite Comic: Green Lantern</p>
-              <p class="text-h6"> Favorite Cereal: Reeses Puffs </p>
-              <p class="text-h6"> Bedtime: During Joe's Lectures </p>
-              <p class="text-h6"> Superpower: Memory Loss </p>
-              <p class="text-h6"> Alias: Dr.Doverylittle </p>
+          <v-col cols="6">
+              <v-img src="../assets/Spencer.png" max-height="200px" contain/>
+              <h3 align="center" class="text-h3">Spencer Schick</h3>
+              <p align="center" class="text-h6">Favorite Comic: Green Lantern</p>
+              <p align="center" class="text-h6"> Favorite Cereal: Reeses Puffs </p>
+              <p align="center" class="text-h6"> Bedtime: During Joe's Lectures </p>
+              <p align="center" class="text-h6"> Superpower: Memory Loss </p>
+              <p align="center" class="text-h6"> Alias: Dr.Doverylittle </p>
           </v-col>
-          <v-col align="center">
-              <h3 class="text-h3">Dustin Dixon</h3>
-              <p class="text-h6">Favorite Comic: Green Lantern</p>
-              <p class="text-h6">Favorite Cereal: Cinnamon Toast Crunch</p>
-              <p class="text-h6">Bedtime: 10:30pm weekdays/before sunrise on the weekends</p>
-              <p class="text-h6">Superpower: Feathered Flight </p>
-              <p class="text-h6">Alias: Masked Macaw</p>
-
-
-
-
-          </v-col>
+          <v-col cols="6">
+              <v-img src="../assets/Dustin.png" max-height="200px" contain/>
+              <h3 align="center" class="text-h3">Dustin Dixon</h3>
+              <p align="center" class="text-h6">Favorite Comic: Green Lantern</p>
+              <p align="center" class="text-h6">Favorite Cereal: Cinnamon Toast Crunch</p>
+              <p align="center" class="text-h6">Bedtime: 10:30pm weekdays/before sunrise on weekends</p>
+              <p align="center" class="text-h6">Superpower: Feathered Flight </p>
+              <p align="center" class="text-h6">Alias: Masked Macaw</p>
+              </v-col>
       </v-row>
   </v-container>
 </template>
