654927901fe487493f7ec52bb659a79f43f7460d
dustin dixon <dustin.a.dixon4@gmail.com>
2021-04-15 20:26:29 -0400

    update about us page

--- vue/src/views/AboutUs.vue
+++ vue/src/views/AboutUs.vue
@@ -1,23 +1,38 @@
 <template>
-  <v-container>
+  <v-container fill-height>
       <v-row>
-          <v-col cols="6">
+          <v-col>
+              <h1 class="text-h1 text-center">ABOUT US</h1>
+          </v-col>
+      </v-row>
+      <v-row align="center">
+          <v-col cols="3">
               <v-img src="../assets/KatComic.png" max-height="200px" contain/>
               <h3 align="center" class="text-h3">Kat Kuntz</h3>
               <p align="center" class="text-h6">Info here</p>
+              <p align="center" class="text-h6">Info here</p>
+              <p align="center" class="text-h6">Info here</p>
+              <p align="center" class="text-h6">Info here</p>
+              <p align="center" class="text-h6">Info here</p>
+              <div class="text-center">
+              <v-btn href="">LinkedIn</v-btn>
+              </div>
           </v-col>
-          <v-col cols="6">
+          <v-divider vertical />
+          <v-col cols="3">
               <v-img src="../assets/Wes.png" max-height="200px" contain/>
               <h3 align="center" class="text-h3">Wesley Davis</h3>
               <p align="center" class="text-h6"> Favorite Comic: Hellboy </p>
               <p align="center" class="text-h6"> Favorite Cereal: Captain Crunch </p>
               <p align="center" class="text-h6"> Bedtime: Too Late </p>
               <p align="center" class="text-h6"> Superpower: Invulnerability </p>
-              <p align="center" class="text-h6"> Alias: Pippin The Hedgehog </p>
+              <p align="center" class="text-h6"> Alias: Pippin Tha Hedgehog </p>
+              <div class="text-center">
+              <v-btn href="https://www.linkedin.com/in/wesley-davis-292150203/" class="mx-auto">LinkedIn</v-btn>
+              </div>
           </v-col>
-      </v-row>
-      <v-row>
-          <v-col cols="6">
+          <v-divider vertical />
+          <v-col cols="3">
               <v-img src="../assets/Spencer.png" max-height="200px" contain/>
               <h3 align="center" class="text-h3">Spencer Schick</h3>
               <p align="center" class="text-h6">Favorite Comic: Green Lantern</p>
@@ -25,8 +40,12 @@
               <p align="center" class="text-h6"> Bedtime: During Joe's Lectures </p>
               <p align="center" class="text-h6"> Superpower: Memory Loss </p>
               <p align="center" class="text-h6"> Alias: Dr.Doverylittle </p>
+              <div class="text-center">
+              <v-btn href="https://www.linkedin.com/in/spencer-schick-2b6402197/" class="mx-auto">LinkedIn</v-btn>
+              </div>
           </v-col>
-          <v-col cols="6">
+          <v-divider vertical />
+          <v-col cols="3">
               <v-img src="../assets/Dustin.png" max-height="200px" contain/>
               <h3 align="center" class="text-h3">Dustin Dixon</h3>
               <p align="center" class="text-h6">Favorite Comic: Green Lantern</p>
@@ -34,6 +53,9 @@
               <p align="center" class="text-h6">Bedtime: 10:30pm weekdays/before sunrise on weekends</p>
               <p align="center" class="text-h6">Superpower: Feathered Flight </p>
               <p align="center" class="text-h6">Alias: Masked Macaw</p>
+              <div class="text-center">
+              <v-btn href="https://www.linkedin.com/in/dustin-dixon-525049191/" class="mx-auto">LinkedIn</v-btn>
+              </div>
               </v-col>
       </v-row>
   </v-container>
